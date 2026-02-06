7d5e0eb424142e8f6aef069bcd956ae7e7c23776
Dan <146668550+NainReg@users.noreply.github.com>
2026-02-06 12:24:44 -0600

    Add files via upload

---
+++ o1/ReolaDifusion16o1.c
@@ -0,0 +1,51 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+
+void setup() {
+    set_flashlight(0);
+    //set_drive_direction(BACKWARD);
+    //set_drive(0);
+    block_value = 0;
+    wait(1000);
+}
+
+void loop() {
+    uint8_t aux1 = get_block_value(929590);
+    uint8_t aux2 = get_block_value(74227);
+    uint8_t aux3 = get_block_value(1370608);
+    uint8_t aux4 = get_block_value(923624);
+    uint8_t aux5 = get_block_value(1376424);
+    uint8_t aux6 = get_block_value(1407280);
+    uint8_t aux7 = get_block_value(1458538);
+    uint8_t aux8 = get_block_value(1460379);
+    
+    uint8_t sum = aux1 + aux2 + aux3 + aux4 + aux5 + aux6 + aux7 + aux8;
+    //Condicional en B2/S7 para:
+    //flashlight cambiando prendido y apagado
+    //rotate cambiando direccion
+    //rotate cambiando gira no  gira
+    if(sum == 2 && block_value == 0){
+        set_flashlight(100);
+        //set_drive_direction(FORWARD);
+        //set_drive(100);
+        block_value = 1;
+    } else if(sum != 2 && block_value == 0){
+        set_flashlight(0);
+        //set_drive_direction(BACKWARD);
+        //set_drive(0);
+        block_value = 0;
+    } else if(sum != 7 && block_value == 1){
+        set_flashlight(0);
+        //set_drive_direction(BACKWARD);
+        //set_drive(0);
+        block_value = 0;
+    } else if(sum == 7 && block_value == 1){
+        set_flashlight(100);
+        //set_drive_direction(FORWARD);
+        //set_drive(100);
+        block_value = 1;
+    }
+
+    wait(1000);
+}
---
+++ o1/ReolaDifusion34o1.c
@@ -0,0 +1,51 @@
+#include "cubelet.h"
+#include <stdint.h>
+#include "routing_table.h"
+
+void setup() {
+    //set_flashlight(0);
+    set_drive_direction(BACKWARD);
+    set_drive(0);
+    block_value = 0;
+    wait(1000);
+}
+
+void loop() {
+    uint8_t aux1 = get_block_value(1376408);//
+    uint8_t aux2 = get_block_value(1460369);//
+    uint8_t aux3 = get_block_value(935718);//
+    uint8_t aux4 = get_block_value(1458561);//
+    uint8_t aux5 = get_block_value(1410451);//
+    uint8_t aux6 = get_block_value(56179);//
+    uint8_t aux7 = get_block_value(1407283);//
+    uint8_t aux8 = get_block_value(816343);//
+    
+    uint8_t sum = aux1 + aux2 + aux3 + aux4 + aux5 + aux6 + aux7 + aux8;
+    //Condicional en B2/S7 para:
+    //flashlight cambiando prendido y apagado
+    //rotate cambiando direccion
+    //rotate cambiando gira no  gira
+    if(sum == 2 && block_value == 0){
+        //set_flashlight(100);
+        //set_drive_direction(FORWARD);
+        set_drive(100);
+        block_value = 1;
+    } else if(sum != 2 && block_value == 0){
+        //set_flashlight(0);
+        //set_drive_direction(BACKWARD);
+        set_drive(0);
+        block_value = 0;
+    } else if(sum != 7 && block_value == 1){
+        //set_flashlight(0);
+        //set_drive_direction(BACKWARD);
+        set_drive(0);
+        block_value = 0;
+    } else if(sum == 7 && block_value == 1){
+        //set_flashlight(100);
+        //set_drive_direction(FORWARD);
+        set_drive(100);
+        block_value = 1;
+    }
+
+    wait(1000);
+}
